903c9b9f1590cc979e72367502e538661dc33a60
Roman <email>
2024-01-13 13:58:49 +0600

    revert accidental code attribution removal

--- ios/DtelecomReactNative.h
+++ ios/DtelecomReactNative.h
@@ -2,6 +2,9 @@
 //  DtelecomReactNative.h
 //  DtelecomReactNative
 //
+//  Created by David Liu on 9/4/22.
+//  Copyright © 2022 LiveKit. All rights reserved.
+//
 
 @interface DtelecomReactNative : NSObject <RCTBridgeModule>
 
